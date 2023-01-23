ebbbc2f0cf3cb8762c073dbb01dab68f8319402a
Piyush <Piyushagrawal826@gmail.com>
2023-01-23 12:31:00 +0530

    adding login end point and password hashing

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {body, validationResult } = require('express-validator')
 const User = require('../models/User')
-
+const bcrypt = require ('bcryptjs')
+var jwt = require('jsonwebtoken')
+const JWT_SECRET ="iamgood$boy"
 ///creating user using POST"/api/auth"
 
 router.post('/createuser' ,[
@@ -24,22 +26,65 @@ try{
   if (user){
     return res.status(400).json({error:"Sorry we can't entertain duplicate emails"})
   }
+  const salt = await bcrypt.genSalt(10)
+  secPass =  await bcrypt.hash(req.body.password,salt) 
   user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
+  name: req.body.name,
+  email: req.body.email,
+  password: secPass
   })
-  // .then(user => res.json(user))
-  // .catch(err=> {console.log(err) 
-  //   res.json({error :"please enter valid details", message:err.message})})
-  res.json(user)
+  const data = {
+    user:{
+      id:user.id
+    }
+  }
+  const authToken = jwt.sign(data,JWT_SECRET)
+  //console.log(jwtData)
+  res.json({authToken})
+  //res.json(user)
 }
 catch(error){
   console.error(error.message);
-  res.status(500).send("some error occured")
+  res.status(500).send("Internal error occured")
 }
 
 })
 
+///Logging a  user using POST"/api/auth"  No login required
+router.post('/login' ,[
+  body('email','Enter a valid email').isEmail(),
+  body('password',"Password can't be blank"),
+ 
+],async (req,res)=>{
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
 
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+const {email, password} = req.body
+try {
+  let user = await User.findOne({email})
+  if(!user){
+    return res.status(400).json({error: "Please try to login with correct credentials"})
+  }
+  const passcomp=  await bcrypt.compare(password,user.password)
+  if(!passcomp){
+    return res.status(400).json({error: "Please try to login with correct credentials"})
+  }
+
+  const payload ={
+    user:{
+      id : user.id
+    }
+  }
+  const authToken = jwt.sign(payload,JWT_SECRET)
+  //console.log(jwtData)
+  res.json({authToken})
+ 
+} catch (error) {
+  console.error(error.message);
+  res.status(500).send("Internal error occured")
+}
+})
 module.exports = router
